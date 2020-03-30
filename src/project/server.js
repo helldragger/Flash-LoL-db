7b138d84ec8d75d47aef4836073b69ffcb017b1b
Helldragger <chritopher1223334444@gmail.com>
2020-03-31 00:36:32 +0200

    add allow CORS requests to server initialization

--- src/project/server.js
+++ src/project/server.js
@@ -50,6 +50,20 @@ var express = require('express');
 var app = express();
 
 app.get('/*', function(req, res) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
     var page = url.parse(req.url).pathname;
     var params = querystring.parse(url.parse(req.url).query);
 
